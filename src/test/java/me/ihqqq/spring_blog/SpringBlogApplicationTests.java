18d6e51c419036f663129de7f1bd324cc06f7017
ihqqq <trnphi127@gmail.com>
2026-03-15 17:04:19 +0700

    chore: initialize project

---
+++ src/test/java/me/ihqqq/spring_blog/SpringBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ihqqq.spring_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
